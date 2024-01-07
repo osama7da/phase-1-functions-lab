9e43ff44b151069936856454078d2379d620a3bf
Osama Daoud <145250456+osama7da@users.noreply.github.com>
2024-01-07 04:06:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    if(street<42){
+        return street -26
+    }else{
+        return street-42;
+
+    }
+}
+
+function distanceFromHqInFeet(street){
+    if(street<42){
+        return (street-26)*264;
+    }else {
+        return (street-42)*264
+    }
+}
+
+function distanceTravelledInFeet(start , destination){
+    if(destination>start){
+        return (destination - start)*264;
+    }else{
+        return (start - destination)*264;
+    }
+    
+}
+
+function calculatesFarePrice(start , destination){
+    let feetDistance = distanceTravelledInFeet(start , destination) ; 
+
+    if(feetDistance <=400){
+        return 0
+    }else if(feetDistance>400 && feetDistance<=2000){
+        return (feetDistance-400) * 0.02
+    }else if(feetDistance>2000 && feetDistance <=2500){
+        return 25 
+    }else{
+        return 'cannot travel that far';
+    }
+}
+
+
